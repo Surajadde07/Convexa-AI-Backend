4bf606a5e15fadbdc704d110034ed1fbeeec8212
Surajadde07 <surajadde57@gmail.com>
2026-05-29 07:02:46 +0530

    Initial Setup

---
+++ src/test/java/com/convexa/ai/convexa_ai_backend/ConvexaAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.convexa.ai.convexa_ai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvexaAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
